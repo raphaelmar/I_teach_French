d149992fccbcf56faa07428ccd1532300705faae
raphaelmar <raphaelmar@hotmail.fr>
2021-03-24 08:53:23 +0000

    created product

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Products, Level
+from .models import Product, Level
 
 # Register your models here.
-admin.site.register(Products)
+admin.site.register(Product)
 admin.site.register(Level)
