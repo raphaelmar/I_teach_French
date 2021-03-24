97b785a8aa3dcfa2f6eaa907f0a144336902a85b
raphaelmar <raphaelmar@hotmail.fr>
2021-03-24 10:42:21 +0000

    created new models and products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Product, Level
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'skill',
+        'difficulty',
+        'description',
+        'price',
+        'rating',
+        'image',
+        'resource',
+    )
+
+    ordering = ('name',)
+
+
+class LevelAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Level, LevelAdmin)
