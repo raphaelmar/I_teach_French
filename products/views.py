51af6f144d032b0f5c3df3ec350b436882757f47
raphaelmar <raphaelmar@hotmail.fr>
2021-03-24 12:24:18 +0000

    display products on products page

--- products/views.py
+++ products/views.py
@@ -7,6 +7,7 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    
 
     context = {
         'products': products,
